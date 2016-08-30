56214fa350e0853e8f756d995dc04ad3ad54d61c
Thomas Venries <thomas.venries@gmail.com>
2016-08-30 17:39:12 +0200

    Add convertor HKCU subkeys into Active Setup reg-add cmd

---
+++ ntlite/registration-entries/wb_activesetup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('registration-entries.xml')
+root = tree.getroot()
+
+f = open('activesetup.bat', 'w')
+
+for entry in root.findall('entry'):
+    keypath = entry.get('name')
+    keyname = keypath.split('\\')[-1]
+    for value in entry.findall('value'):
+        valuename = value.get('name')
+        data = value.find('data').text
+        datatype = value.find('dataType').text
+        separator = value.find('separator').text
+        cmd = ''.join(['reg add ',
+                '"HKLM\SOFTWARE\Microsoft\Active Setup\Installed Components\\',
+                keyname, '-', valuename,
+                '" ^\n\t/v "Version" ^\n\t/d "1" /t ', datatype,
+                ' /f\n', 'reg add ',
+                '"HKLM\SOFTWARE\Microsoft\Active Setup\Installed Components\\',
+                keyname, '-', valuename,
+                '" ^\n\t/v "StubPath" ^\n\t/d "reg add ',
+                keypath, ' /v "', valuename, '" /d "', data, '" /t ', datatype,
+                " /f\" ^\n\t/f\n"])
+        f.write(cmd)
+    f.write("\n\n")
