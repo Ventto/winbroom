85a59e4c07869240477c09bea466a6901fb1593e
Thomas Venries <thomas.venries@gmail.com>
2016-09-03 21:06:04 +0200

    activesetup: Factorization

--- ntlite/registration-entries/wb_activesetup.py
+++ ntlite/registration-entries/wb_activesetup.py
@@ -2,7 +2,7 @@
 
 from xml.etree.ElementTree import ParseError
 import os
-import sys
+import sys.exit
 import xml.etree.ElementTree as ET
 
 def get_status(filepath):
@@ -11,40 +11,44 @@ def get_status(filepath):
             sys.exit(1)
         return filepath
 
-if len(sys.argv) != 2:
-    print('Usage: wb_activesetup.py file.XML \n', file=sys.stderr)
-    exit(0)
+def main(argv):
+    if len(argv) != 2:
+        print('Usage: wb_activesetup.py file.XML \n', file=sys.stderr)
+        sys.exit(0)
 
-try:
-    tree = ET.parse(get_status(sys.argv[1]))
-except ParseError:
-    tree = None
+    try:
+        tree = ET.parse(get_status(argv[1]))
+    except ParseError:
+        tree = None
 
-if tree == None:
-    print('Error: Bad XML syntax.\n', file=sys.stderr)
-    exit(1)
+    if tree == None:
+        print('Error: Bad XML syntax.\n', file=sys.stderr)
+        sys.exit(1)
 
-root = tree.getroot()
+    root = tree.getroot()
 
-batfile = open('activesetup.bat', 'w')
+    batfile = open('activesetup.bat', 'w')
 
-for entry in root.findall('entry'):
-    keypath = entry.get('name')
-    keyname = keypath.split('\\')[-1]
+    for entry in root.findall('entry'):
+        keypath = entry.get('name')
+        keyname = keypath.split('\\')[-1]
 
-    for value in entry.findall('value'):
-        valuename = value.get('name')
-        data = value.find('data').text
-        datatype = value.find('dataType').text
-        cmd = ''.join(['reg add ',
-                '"HKLM\SOFTWARE\Microsoft\Active Setup\Installed Components\\',
-                keyname, '-', valuename,
-                '" ^\n\t/v "Version" ^\n\t/d "1" /f\n reg add ',
-                '"HKLM\SOFTWARE\Microsoft\Active Setup\Installed Components\\',
-                keyname, '-', valuename,
-                '" ^\n\t/v "StubPath" ^\n\t/d "reg add ',
-                keypath, ' /v "', valuename, '" /d "', data, '" /t ', datatype,
-                " /f\" ^\n\t/f\n"])
-        batfile.write(cmd)
+        for value in entry.findall('value'):
+            valuename = value.get('name')
+            data = value.find('data').text
+            datatype = value.find('dataType').text
+            cmd = ''.join(['reg add ',
+                    '"HKLM\SOFTWARE\Microsoft\Active Setup\Installed Components\\',
+                    keyname, '-', valuename,
+                    '" ^\n\t/v "Version" ^\n\t/d "1" /f\n reg add ',
+                    '"HKLM\SOFTWARE\Microsoft\Active Setup\Installed Components\\',
+                    keyname, '-', valuename,
+                    '" ^\n\t/v "StubPath" ^\n\t/d "reg add ',
+                    keypath, ' /v "', valuename, '" /d "', data, '" /t ', datatype,
+                    " /f\" ^\n\t/f\n"])
+            batfile.write(cmd)
 
-    batfile.write("\n\n")
+        batfile.write("\n\n")
+
+if __name__ == "__main__":
+    main(sys.argv)
